c8b0735097556000f933dec2d45eca8ac67fa8cd
Gayathriss24 <srishbloosom@gmail.com>
2024-09-10 19:44:27 +0530

    Add files via upload
    Day2

---
+++ day2/Chrome.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version=9.1f;
+		String developer="Google";
+		boolean isBeta=false;
+		int releaseYear=2008;
+		char shortcutKey='C';
+		System.out.println("Version type: "+version+"\nDeveloper of string:"+developer+"\nType boolean: "+isBeta+"\nRelease year: "+releaseYear+"\nShortcut Key: "+shortcutKey);
+
+	}
+
+}
